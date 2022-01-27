54b9cad84fb9f3cbedfe3fa3bb95c8cfc3b4f424
Nicolas Ibarra <niicoibarra1@gmail.com>
2022-01-27 15:36:15 -0300

    Moving pieces problem resolved

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ console.log(SQUARES);
 //Tetris pieces. Each field of an array is a different position of a piece. It's based on: 
 /* [0,0]  [0,1]  [0,2]
    [1,0]  [1,1]  [1,2]
-   [2,0]  [2,1]  [2,2]  */
+   [2,0]  [2,1]  [2,2]  
+   [3,0]  [3,1]  [3,2]  */
 
 const PIECE_L = [
   [1, WIDTH+1, WIDTH*2+1, 2],
@@ -29,7 +30,7 @@ const PIECE_L = [
 
 const PIECE_Z = [
   [WIDTH*2, WIDTH*2+1, WIDTH+1, WIDTH+2],
-  [0, WIDTH, WIDTH+1, WIDTH*2+1]
+  [0, WIDTH, WIDTH+1, WIDTH*2+1],
   [WIDTH*2, WIDTH*2+1, WIDTH+1, WIDTH+2],
   [0, WIDTH, WIDTH+1, WIDTH*2+1]
 ]
@@ -58,9 +59,9 @@ const PIECE_I = [
 //Painting the pieces
 const PIECES = [PIECE_L, PIECE_Z, PIECE_T, PIECE_O, PIECE_I];
 let currentPosition = 3; //Start position at X axis in the grid
-const RANDOM_PIECE = Math.floor(Math.random() * PIECES.length); //Randomly select a piece
+let randomPiece = Math.floor(Math.random() * PIECES.length); //Randomly select a piece
 let pieceRotation = 0; //The pieces will always start with their first rotation
-let currentPiece = PIECES[RANDOM_PIECE][pieceRotation]; //The piece shown in the grid
+let currentPiece = PIECES[randomPiece][pieceRotation]; //The piece shown in the grid
 
 function drawPiece(){
   currentPiece.forEach(item => { //Each div from SQUARES array is painted, according to the selected piece
@@ -79,12 +80,12 @@ function undrawPiece(){
 function freeze(){
   if(currentPiece.some(item => SQUARES[currentPosition + item + WIDTH].classList.contains('taken'))){ //Evaluate if the piece has reached the end of the grid
     currentPiece.forEach(item => SQUARES[currentPosition + item].classList.add('taken'));
-    console.log(RANDOM_PIECE)
+    console.log(randomPiece)
 
     //Initialize a new piece in the grid
     pieceRotation = 0;
-    const NEW_RANDOM_PIECE = Math.floor(Math.random() * PIECES.length);
-    currentPiece = PIECES[NEW_RANDOM_PIECE][pieceRotation];
+    randomPiece = Math.floor(Math.random() * PIECES.length);
+    currentPiece = PIECES[randomPiece][pieceRotation];
     currentPosition = 3;
     drawPiece();
   }
@@ -100,10 +101,9 @@ function movesControl(e){
 document.addEventListener('keyup', movesControl);
 
 //Moving the pieces
-let timerId = setInterval(moveDown, 1000); //Every 1 second, the piece will go down
+let timerId = setInterval(moveDown, 500); //Every 1 second, the piece will go down
 
 function moveDown(){
-  console.log(PIECES[RANDOM_PIECE][pieceRotation])
   undrawPiece();
   currentPosition += WIDTH;
   drawPiece();
@@ -114,7 +114,7 @@ function moveLeft(){
   undrawPiece();
   const LEFT_LIMIT = currentPiece.some(item => (currentPosition + item) % WIDTH === 0); //Determine the left limit of the grid by dividing the current array position and analizing the reminder
   if(!LEFT_LIMIT) {currentPosition -= 1;} //If the piece is not already in the left limit, it can moves to the left
-  if(currentPiece.some(item => SQUARES[currentPiece + item].classList.contains('taken'))) {currentPosition -= 1;} //If the piece is on the left limit, it goes back to the right
+  if(currentPiece.some(item => SQUARES[currentPosition + item].classList.contains('taken'))) {currentPosition -= 1;} //If the piece is on the left limit, it goes back to the right
   drawPiece();
 }
 
@@ -122,14 +122,14 @@ function moveRight(){
   undrawPiece();
   const RIGHT_LIMIT = currentPiece.some(item => (currentPosition + item) % WIDTH === WIDTH - 1); //Determine the right limit of the grid by dividing the current array position and analizing the reminder
   if(!RIGHT_LIMIT) {currentPosition += 1;} //If the piece is not already in the right limit, it can moves to the right
-  if(currentPiece.some(item => SQUARES[currentPiece + item].classList.contains('taken'))) {currentPosition -= 1;} //If the piece is on the left limit, it goes back to the right
+  if(currentPiece.some(item => SQUARES[currentPosition + item].classList.contains('taken'))) {currentPosition -= 1;} //If the piece is on the left limit, it goes back to the right
   drawPiece();
 }
 
 function rotate(){
   undrawPiece();
   pieceRotation++;
-  if(pieceRotation === currentPiece.length){pieceRotation = 0;}
-  currentPiece = PIECES[RANDOM_PIECE][pieceRotation];
+  if(pieceRotation === currentPiece.length){pieceRotation = 0;} //If the rotation is the last one of the piece (position 4), then it goes back to position 0.
+  currentPiece = PIECES[randomPiece][pieceRotation];
   drawPiece();
 }
