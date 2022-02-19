d1ab6dba0628077d5b802be719a0df4c371fbfb4
Nicolas Ibarra <niicoibarra1@gmail.com>
2022-02-19 18:52:32 -0300

    Comments of things to improve

--- app.js
+++ app.js
@@ -7,20 +7,26 @@
   }
 } */
 
-const WIDTH = 10; //The current quantity of cells in a row of the grid
-const GRID = document.querySelector(".grid"); 
-let squares = Array.from(document.querySelectorAll(".grid div")); //We pick all the divs inside the grid and make an array of divs
-const SCORE_DISPLAY = document.getElementById("score");
+let timerId;
 let score = 0;
+let squares = Array.from(document.querySelectorAll(".grid div")); //We pick all the divs inside the grid and make an array of divs.
+let currentPosition = 3; //Current position cell of the pieces in the grid. It starts in cell 3.
+let pieceRotation = 0; //The pieces will always start with their first rotation.
+
+const WIDTH = 10; //The current quantity of cells in a row of the grid.
+const GRID = document.querySelector(".grid"); 
+const SCORE_DISPLAY = document.getElementById("score");
+const COLORS = ['purple', 'red', 'orange', 'lightblue', 'green'];
 const BTN_START = document.getElementById("start-button");
 
-console.log(squares);
+//Tetris pieces -> Each field of the pieces arrays is a different position of the piece. It's based on: 
 
-//Tetris pieces. Each field of an array is a different position of a piece. It's based on: 
-/* [0,0]  [0,1]  [0,2]
-   [1,0]  [1,1]  [1,2]
-   [2,0]  [2,1]  [2,2]  
-   [3,0]  [3,1]  [3,2]  */
+/*
+[0,0]  [0,1]  [0,2]
+[1,0]  [1,1]  [1,2]
+[2,0]  [2,1]  [2,2]  
+[3,0]  [3,1]  [3,2]
+*/
 
 const PIECE_L = [
   [1, WIDTH+1, WIDTH*2+1, 2],
@@ -57,36 +63,35 @@ const PIECE_I = [
   [WIDTH, WIDTH+1, WIDTH+2, WIDTH+3]
 ]
 
-//Painting the pieces
-const PIECES = [PIECE_L, PIECE_Z, PIECE_T, PIECE_O, PIECE_I];
-let currentPosition = 3; //Start position at X axis in the grid
-let randomPiece = Math.floor(Math.random() * PIECES.length); //Randomly select a piece
-let nextRandomPiece = randomPiece;
-let pieceRotation = 0; //The pieces will always start with their first rotation
-let currentPiece = PIECES[randomPiece][pieceRotation]; //The piece shown in the grid
-const COLORS = ['purple', 'red', 'orange', 'lightblue', 'green']; 
+const PIECES = [PIECE_L, PIECE_Z, PIECE_T, PIECE_O, PIECE_I]; //Array of pieces.
 
+let randomPiece = Math.floor(Math.random() * PIECES.length); //Randomly select a piece.
+let currentPiece = PIECES[randomPiece][pieceRotation]; //The current piece shown in the grid.
+let nextRandomPiece = randomPiece; //This variable is used for generating the next tetrominoe in the grid.
+
+document.addEventListener('keyup', movesControl);
+BTN_START.addEventListener('click', manageMovement);
+
+//PAINTING THE PIECES
 function drawPiece(){
-  currentPiece.forEach(item => { //Each div from SQUARES array is painted, according to the selected piece
-    squares[currentPosition + item].classList.add('piece'); //The class 'piece' is added to each div to gain color
+  currentPiece.forEach(item => { //Each div that makes up a piece is painted, according to the selected piece.
+    squares[currentPosition + item].classList.add('piece'); //The class 'piece' is added to each div to gain color.
     squares[currentPosition + item].style.backgroundColor = COLORS[randomPiece];
   })
 }
 
-//Unpainting the pieces
+//UNPAINTING THE PIECES
 function undrawPiece(){
   currentPiece.forEach(item => {
-    squares[currentPosition + item].classList.remove('piece'); //The class 'piece' is removed from each div
+    squares[currentPosition + item].classList.remove('piece'); //The class 'piece' is removed from each div.
     squares[currentPosition + item].style.backgroundColor = '';
   })
 }
 
-//Freezing the pieces at the end of the grid
+//FREEZING A PIECE WHEN IT REACHES THE BOTTOM OF THE GRID OR TOUCHES ANOTHER PIECE.
 function freeze(){
-  if(currentPiece.some(item => squares[currentPosition + item + WIDTH].classList.contains('taken'))){ //Evaluate if the piece has reached the end of the grid
-    currentPiece.forEach(item => squares[currentPosition + item].classList.add('taken'));
-
-    addScore(); //This function must be called in here. Otherwise, before the complete rows are cut, the next piece appears in the grid and its dragged down. 
+  if(currentPiece.some(item => squares[currentPosition + item + WIDTH].classList.contains('taken'))){ //Evaluate if the piece has reached the end of the grid.
+    currentPiece.forEach(item => squares[currentPosition + item].classList.add('taken')); //The piece stops moving.
 
     //Initialize a new piece in the grid
     pieceRotation = 0;
@@ -94,12 +99,13 @@ function freeze(){
     nextRandomPiece = Math.floor(Math.random() * PIECES.length);
     currentPiece = PIECES[randomPiece][pieceRotation];
     currentPosition = 3;
+    addScore(); //This function must be called in here. Otherwise, before the complete rows are cut, the next piece appears in the grid and its dragged down. 
     drawPiece();
     displayPiece();
   }
 }
 
-//Manging the moves -------> REVISAR
+//MANAGING THE MOVEMENTS -------> REVISAR
 function movesControl(e){
   if(e.keyCode === 37) {moveLeft();}
   if(e.keyCode === 39) {moveRight();}
@@ -107,12 +113,8 @@ function movesControl(e){
   if(e.keyCode === 40) {moveDown();}
 }
 
-document.addEventListener('keyup', movesControl);
-
-//Moving the pieces 
-let timerId;
-
-function manageMovement(){ //This function manage the Time/Pause button
+//MANAGING THE TIME/PAUSE FUNCTIONALITY
+function manageMovement(){ 
   if(timerId){
     clearInterval(timerId);
     timerId = null;
@@ -122,6 +124,7 @@ function manageMovement(){ //This function manage the Time/Pause button
   }
 }
 
+//MANAGING THE DIFFERENT FUNCTIONALITIES
 function moveDown(){
   displayPiece();
   undrawPiece();
@@ -130,6 +133,7 @@ function moveDown(){
   freeze();
   gameOver();
 }
+
 //REVISAR AMBAS FUNCIONES
 function moveLeft(){
   undrawPiece();
@@ -147,20 +151,22 @@ function moveRight(){
   drawPiece();
 }
 
+//MANAGING THE DIFFERENT ROTATIONS OF A PIECE
 function rotate(){
   undrawPiece();
   pieceRotation++;
-  if(pieceRotation === currentPiece.length){pieceRotation = 0;} //If the rotation is the last one of the piece (position 4), then it goes back to position 0.
+  if(pieceRotation === currentPiece.length){ //If the rotation is the last one of the piece (position 4), then it goes back to position 0.
+    pieceRotation = 0; 
+  } 
   currentPiece = PIECES[randomPiece][pieceRotation];
   drawPiece();
 }
 
-//Mini grid
+//MINI GRID
 const MINI_WIDTH = 5;
 const MINI_DISPLAY = document.querySelectorAll('.mini-grid div');
 const DISPLAY_INDEX = 1;
 
-//Mini tetrominoes
 const MINI_PIECES = [
   [1, MINI_WIDTH+1, MINI_WIDTH*2+1, 2],
   [MINI_WIDTH*2, MINI_WIDTH*2+1, MINI_WIDTH+1, MINI_WIDTH+2],
@@ -169,7 +175,7 @@ const MINI_PIECES = [
   [1, MINI_WIDTH+1, MINI_WIDTH*2+1, MINI_WIDTH*3+1]
 ]
 
-//Display the shape of the next tetrominoe
+//DISPLAY THE SHAPE OF THE NEXT TETROMINOE
 function displayPiece(){
   MINI_DISPLAY.forEach(item => {
     item.classList.remove('piece'); //Remove any trace of the previous tetrominoe
@@ -182,14 +188,12 @@ function displayPiece(){
   })
 }
 
-BTN_START.addEventListener('click', manageMovement);
-
-//Adding score function
+//SCORE FUNCTIONALITY
 function addScore(){
   for(let i=0; i<199; i+=WIDTH){
     const ROW = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]; //Creating an array with the length of WIDTH for analizing a single row. Its necessary to add the constants in each cell so the array will represent the rows of the grid.
 
-    if(ROW.every( i => squares[i].classList.contains('taken'))){ //We ask if the row in 'i' position of the grid has all its cells with the classList 'taken'. 
+    if(ROW.every( i => squares[i].classList.contains('taken'))){ //We ask if the row has all its cells with the classList 'taken'. 
       score += 10;
       SCORE_DISPLAY.innerHTML = score;
 
@@ -207,10 +211,23 @@ function addScore(){
   }
 }
 
-//Game over function
+//GAME OVER FUNCTIONALITY
 function gameOver(){
   if (currentPiece.some(item => squares[item + currentPosition].classList.contains('taken'))){ //If the tetrominoe has riched the top of the grid then the interval is cleared and stops the pieces movement.
     SCORE_DISPLAY.innerHTML = "YOU LOST!" + " Your final score is " + score;
     clearInterval(timerId);
   }
 }
+
+/* 
+
+CONSIDERACIONES
+* Acomodar la posicion de variables al inicio del archivo .js, incluyendo las constantes del MINI GRID.
+* Eliminar la clase 'piece' del archivo .css. Ya no es necesaria. Los colores se generan aleatoriamente.
+* Hacer una nueva función con líneas repetidas de inicialización de nuevas piezas, de posición, etc.
+* Revisar la función movesControl. Cambiar estructura sin tantos ifs.
+* Revisar si la asignación en la función moveDown se puede poner dentro de otra función.
+* Revisar funciones moveRight y moveLeft. Cambiar lógica.
+* Separar la función addScore en distintas funciones.
+
+*/
